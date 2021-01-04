6adba6446c4a857cd7faeed56d38d08fa1dbb79b
Akshay Mathur <akshay@dotimely.com>
2021-01-04 17:48:01 +0530

    implement deck of cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+
+import React, { Component }  from 'react';
+
+import Spade from './images/Spade.png';
+import Club from './images/Club.png';
+import Heart from './images/Heart.png';
+import Diamond from './images/Diamond.png';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { connect } from 'react-redux';
+
+const CARDS_TO_DRAW = 5;
+
+const createDeck = (deck = []) => {
+  let suits = ['Club', 'Hearts', 'Spade', 'Diamonds'];
+  let numbers = ['1', '2', '3', '4' , '5', '6', '7' , '8' , '9', 'J', 'Q', 'K', 'A'];
+
+  for(let i = 0; i < suits.length; i++) {
+    for(let j = 0; j < numbers.length; j++) {
+
+      let card = {};
+      card.suit = suits[i];
+      card.number = numbers[j];
+      deck.push(card);
+    }
+  }
+  return deck;
 }
 
-export default App;
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      deck: createDeck([]),
+      drawnDeck: []
+    }
+  }
+  
+  drawCards = () => {
+    const  { deck, drawnDeck } = this.state;
+    let card;
+    
+    for(let i = 0; i < CARDS_TO_DRAW; i++) {
+      if( deck.length > 0 ) {
+        let randIndex = Math.floor( Math.random() * deck.length );
+        card = deck.splice( randIndex, 1 )[0];
+        drawnDeck.push(card);
+      } else {
+        alert('deck empty');
+      }
+    }
+    this.setState({deck: deck, drawnDeck: drawnDeck  })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={() => this.drawCards()}> Draw </button>
+        <DrawnCards drawnDeck={this.state.drawnDeck}></DrawnCards>
+      </div>
+    );
+  }
+}
+
+function DrawnCards ({ drawnDeck }) {
+  return (
+    <div>
+      {
+        drawnDeck.map(card => {          
+            return (
+              <div class="card-container">
+                <div className="number">{card.number}</div>
+                { card.suit === 'Club' && <img  class="card-img" src={Club} alt="Club"></img>}
+                { card.suit === 'Hearts' && <img  class="card-img" src={Heart} alt="Heart"></img>}
+                { card.suit === 'Spade' && <img  class="card-img" src={Spade} alt="Spade"></img>}
+                { card.suit === 'Diamonds' && <img  class="card-img" src={Diamond} alt="Diamond"></img>}
+                
+              </div>
+            );
+        })
+      }
+    </div>
+  )
+}
+
+
+
+export default connect()(App)
